55664bee867328e453a136a5ff0cd5d25cd3ae98
Leonardo igor <bufige@yahoo.com>
2020-08-17 08:46:29 -0300

    Updates

---
+++ precache-manifest.be34f804c8ce93d1e6a2ebc7a834fadf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020e5051920bb668477510fab99d8f9f",
+    "url": "/Joblister/index.html"
+  },
+  {
+    "revision": "d9c5c7ef372b1663eaa2",
+    "url": "/Joblister/static/js/2.a7ee1178.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Joblister/static/js/2.a7ee1178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4092430386e367a9a8a7",
+    "url": "/Joblister/static/js/main.42136f4f.chunk.js"
+  },
+  {
+    "revision": "e324afb35d6df6c5c137",
+    "url": "/Joblister/static/js/runtime-main.2ef7971e.js"
+  }
+]);
