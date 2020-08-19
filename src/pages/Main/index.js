2c498a3dc4105634574e361ff24d7a7ab1077216
Leonardo igor <bufige@yahoo.com>
2020-08-19 12:08:34 -0300

    removed debug messages

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -8,7 +8,6 @@ import {Container, Loading} from './styles';
 import {useStoreContext} from '../../storeContext';
 export default function Tag(props) {
 	const {loading} = useStoreContext();
-	console.log(loading)
     return loading ? <Loading><i class="fas fa-spinner fa-spin fa-4x"></i></Loading> :  <Container>
 		<Search />
 		<Jobs/>
